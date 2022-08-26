e150f73d72b103b66ef0ac4794655d4abecb21d7
Thiago Fernandes <thiago.fernan.dev@gmail.com>
2022-08-26 14:35:32 -0300

    Prática 1

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -10,4 +10,10 @@ console.log("Profissão: ",profissao)
 console.log("Idade: ",idade)
 console.log("Lugar que desejo conhecer: ",lugar)
 
+console.log("**********************************************")
+// Desafio 1
+
+console.log("Olá,",nome,"! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para",lugar,". Confirma pra gente alguns dados? A sua idade é ",idade,", você é",profissao,"e o seu email é",email,"? Ficamos no aguardo. Muito obrigado e boa viagem! :D")
+
+
 
