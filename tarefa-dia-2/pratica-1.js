92e0c477cd05eb941df1fe058cfe9724a2b3f95d
Thiago Fernandes <thiago.fernan.dev@gmail.com>
2022-08-26 14:24:18 -0300

    Prática 1

---
+++ tarefa-dia-2/pratica-1.js
@@ -0,0 +1,13 @@
+let nome = "Thiago"
+let email = "thiago.fernan21@hotmail.com"
+let profissao = "Estudante"
+let idade = 25
+let lugar = "California"
+
+console.log("Nome: ",nome)
+console.log("Email: ",email)
+console.log("Profissão: ",profissao)
+console.log("Idade: ",idade)
+console.log("Lugar que desejo conhecer: ",lugar)
+
+
