c5fd216cc0b5ac926b32f20ce3792bf54254f818
Thiago Fernandes <thiago.fernan.dev@gmail.com>
2022-08-28 19:05:51 -0300

    tarefa-dia-3

---
+++ Teste-dia-3.js
@@ -0,0 +1,15 @@
+let nome = "Thiago"
+let idade = 25
+if (idade >=18) {
+    console.log(`${nome} entrada liberada`);
+} else {
+    console.log(`${nome} Acesso negado`);
+}
+console.log("********** PAR OU IMPAR**********")
+
+let numero = 8
+if (numero % 2 == 0) {
+    console.log(`${numero} é par`)
+} else {
+    console.log(`${numero} é ímpar`)
+}
